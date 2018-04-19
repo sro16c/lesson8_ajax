61b7f1b33feef8e3c2898802fd9be0543a4c5ab6
Sean Oneill <sro16c@my.fsu.edu>
2018-04-19 02:15:32 +0000

    Lesson 8 final commit

--- Movie.js
+++ Movie.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
 
             $.ajax({
                 //Insert YOUR API Key after the '=' inside the single quotes.
-                url:'https://api.themoviedb.org/3/search/movie?api_key=INSERT-HERE',
+                url:'https://api.themoviedb.org/3/search/movie?api_key=9923d8f925a9d911a3ed67c0a9323cec',
                 data: query
             })
             
@@ -58,6 +58,7 @@ $(document).ready(function(){
                  *********************************Insert Code Below********************************************/                
                 
                 
+                $("#overview").html(json.results[0].overview);
                 
 
                 /***********************Retrieve Movie Release Date from Server & Add to HTML Document***********
@@ -72,7 +73,7 @@ $(document).ready(function(){
                  
                  *********************************Insert Code Below********************************************/               
 
-
+                $("#release_date").html(json.results[0].release_date);
 
 
                  /***********************Retrieve Movie Popularity Rate from Server & Add to HTML Document******
@@ -87,7 +88,7 @@ $(document).ready(function(){
                  
                  *********************************Insert Code Below********************************************/   
 
-  
+                $("#popularity").html(json.results[0].popularity);
 
                 
                  /*****************Retrieve Movie Original Language from Server & Add to HTML Document***********
@@ -102,7 +103,7 @@ $(document).ready(function(){
                  
                  *********************************Insert Code Below********************************************/                 
 
-
+                $("#original_language").html(json.results[0].original_language);
 
                 //The following code retrieves the movie poster path from the JSON object retrieved from the TMDB 
                 //Server and places the poster image on the HTML page, where the id="poster_path".  It took TWO
